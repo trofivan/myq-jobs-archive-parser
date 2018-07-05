df713d0c1aff3de59d733310cbb94e526dbf6b6f
Ivan Trofimov <ivan@trofimov.link>
2018-07-05 11:28:18 +0500

    feat(JobsTable container): add getVisibleJobs() to the mapStateToProps for display only filtered elements

--- src/containers/JobsTable.js
+++ src/containers/JobsTable.js
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
 import JobsTable from '../components/Content/JobsTable';
 
+const getVisibleJobs = (jobs = [], jobsType = []) =>
+  jobs.filter(job => !job.error && jobsType.includes(job.jobType));
+
 const mapStateToProps = state => ({
-  jobs: state.jobs.list.filter(job => !job.error)
+  jobs: getVisibleJobs(state.jobs.list, state.filter.jobsType)
 });
 
-const mapDispatchToProps = dispatch => ({
-  
-});
+const mapDispatchToProps = dispatch => ({});
 
 export default connect(
   mapStateToProps,
