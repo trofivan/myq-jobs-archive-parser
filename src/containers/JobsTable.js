9d829f038c6c667366eb938de086dba60a1be069
Ivan Trofimov <ivan@trofimov.link>
2018-07-06 12:32:42 +0500

    refactor(JobsTable): add function filterJobs(jobs = [], ...funcs)

--- src/containers/JobsTable.js
+++ src/containers/JobsTable.js
@@ -1,17 +1,23 @@
 import { connect } from 'react-redux';
 import JobsTable from '../components/Content/JobsTable';
 
-const getVisibleJobs = (jobs = [], types = [], users = []) =>
-  jobs
-    .filter(job => !job.error)
-    .filter(job => types.includes(job.jobType))
-    .filter(job => (users.length === 0 ? true : users.includes(job.username)));
+const filterJobs = (jobs = [], ...funcs) =>
+  funcs.reduce((acc, fn) => fn(acc), jobs);
+
+const filterNoErrors = jobs => jobs.filter(job => !job.error);
+const filterByType = types => jobs =>
+  jobs.filter(job => types.includes(job.jobType));
+const filterByUsers = users => jobs =>
+  jobs.filter(
+    job => (users.length === 0 ? true : users.includes(job.username))
+  );
 
 const mapStateToProps = state => ({
-  jobs: getVisibleJobs(
+  jobs: filterJobs(
     state.jobs.list,
-    state.filter.jobsType,
-    state.filter.users
+    filterNoErrors,
+    filterByType(state.filter.jobsType),
+    filterByUsers(state.filter.users)
   )
 });
 
