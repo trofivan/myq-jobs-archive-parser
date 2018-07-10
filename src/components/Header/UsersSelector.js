6992605cabf261ec8a94aaf7e313d0850ac56c27
Ivan Trofimov <ivan@trofimov.link>
2018-07-10 12:52:27 +0500

    refactor(UsersSelector): delete old code

--- src/components/Header/UsersSelector.js
+++ src/components/Header/UsersSelector.js
@@ -2,25 +2,22 @@ import React from 'react';
 import { Select } from 'antd';
 const { Option } = Select;
 
-export default ({ isDisabled, users, selectedUsers, onChangeUsers }) => {
-  console.log(selectedUsers);
-  return (
-    <Select
-      showSearch
-      style={{ width: '100%' }}
-      placeholder="Users"
-      mode="multiple"
-      optionFilterProp="children"
-      allowClear
-      value={selectedUsers}
-      disabled={isDisabled}
-      onChange={onChangeUsers}
-    >
-      {users.map((user, idx) => (
-        <Option key={idx} value={user}>
-          {user}
-        </Option>
-      ))}
-    </Select>
-  );
-};
+export default ({ isDisabled, users, selectedUsers, onChangeUsers }) => (
+  <Select
+    showSearch
+    style={{ width: '100%' }}
+    placeholder="Users"
+    mode="multiple"
+    optionFilterProp="children"
+    allowClear
+    value={selectedUsers}
+    disabled={isDisabled}
+    onChange={onChangeUsers}
+  >
+    {users.map((user, idx) => (
+      <Option key={idx} value={user}>
+        {user}
+      </Option>
+    ))}
+  </Select>
+);
