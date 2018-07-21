b5b51c9e23abfe8d6becf2f3a9a427f4772cb57a
Ivan Trofimov <ivan@trofimov.link>
2018-07-21 13:49:30 +0500

    feat(fetchJobs middleware): added terminator for fetchJobs (action FETCH_JOBS_CANCEL)

--- src/middlewares/fetchJobs.js
+++ src/middlewares/fetchJobs.js
@@ -1,20 +1,38 @@
-import Worker from '../workers/fetchJobs.worker';
-import { FETCH_JOBS_REQUEST } from '../constants/actionTypes';
-import { fetchJobsSuccess } from '../actions';
+import FetchJobsWorker from '../workers/fetchJobs.worker';
+import {
+  FETCH_JOBS_REQUEST,
+  FETCH_JOBS_CANCEL
+} from '../constants/actionTypes';
+import { fetchJobsSuccess, fetchJobsError } from '../actions';
+
+const handleMessage = dispatch => msg => {
+  const jobs = msg.data;
+  dispatch(fetchJobsSuccess(jobs));
+};
+
+const handleError = dispatch => err => {
+  dispatch(fetchJobsError(err));
+};
+
+const initWorker = dispatch => {
+  const worker = new FetchJobsWorker();
+  worker.addEventListener('message', handleMessage(dispatch), false);
+  worker.addEventListener('error', handleError(dispatch), false);
+  return worker;
+};
 
 export default ({ dispatch }) => {
-  const handleMessage = msg => {
-    const jobs = msg.data;
-    dispatch(fetchJobsSuccess(jobs));
-  };
-
-  const fetchJobsWorker = new Worker();
-  fetchJobsWorker.addEventListener('message', handleMessage, false);
+  let fetchJobsWorker = initWorker(dispatch);
 
   return next => action => {
     if (action.type === FETCH_JOBS_REQUEST) {
       const { jobsFolder } = action;
       fetchJobsWorker.postMessage(jobsFolder);
+    }
+
+    if (action.type === FETCH_JOBS_CANCEL) {
+      fetchJobsWorker.terminate();
+      fetchJobsWorker = initWorker(dispatch);
     }
 
     return next(action);
