d1399df5fb727bdcade803199caa7fd5706cbc77
Ivan Trofimov <ivan@trofimov.link>
2018-08-17 15:33:39 +0500

    fix(JobsTable): tranlatiration Job type column

--- src/components/Content/JobsTable/RenderJobTypeColumn.js
+++ src/components/Content/JobsTable/RenderJobTypeColumn.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Icon } from 'antd';
+import { FormattedMessage } from 'react-intl';
 
 const RenderJobTypeColumn = jobType => {
   switch (jobType) {
     case 'print':
-      return <Icon type="printer" title="Print" />;
+      return (
+        <FormattedMessage id="App.content.jobsTable.data.type.print">
+          {titleMsg => <Icon type="printer" title={titleMsg} />}
+        </FormattedMessage>
+      );
     case 'scan':
-      return <Icon type="profile" title="Scan" />;
+      return (
+        <FormattedMessage id="App.content.jobsTable.data.type.scan">
+          {titleMsg => <Icon type="profile" title={titleMsg} />}
+        </FormattedMessage>
+      );
     case 'copy':
-      return <Icon type="copy" title="Copy" />;
+      return (
+        <FormattedMessage id="App.content.jobsTable.data.type.copy">
+          {titleMsg => <Icon type="copy" title={titleMsg} />}
+        </FormattedMessage>
+      );
     default:
       return '';
   }
