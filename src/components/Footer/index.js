55dde1f6676cbb57367ebea88275d70574fbc381
Ivan Trofimov <ivan@trofimov.link>
2018-08-21 11:08:58 +0500

    feat(Footer): external URLs as Components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
 import { Row, Col, Icon } from 'antd';
 import { name as appName, version as appVer } from '../../../package.json';
+import {
+  URL_LATEST_RELEASE,
+  URL_LICENSE,
+  URL_PROJECT
+} from '../../constants/externalURLs';
 
 const styleRow = {
   padding: '16px 0',
   backgroundColor: '#ffffff'
 };
+
+const UrlLicense = () => (
+  <a
+    href="#License"
+    onClick={e => {
+      e.preventDefault();
+      nw.Shell.openExternal(URL_LICENSE);
+    }}
+  >
+    MIT License
+  </a>
+);
+
+const UrlProject = () => (
+  <a
+    href="#GitHub"
+    onClick={e => {
+      e.preventDefault();
+      nw.Shell.openExternal(URL_PROJECT);
+    }}
+  >
+    {appName}
+  </a>
+);
+
+const UrlLatestRelease = () => (
+  <a
+    href="#LatestRelease"
+    onClick={e => {
+      e.preventDefault();
+      nw.Shell.openExternal(URL_LATEST_RELEASE);
+    }}
+  >
+    v{appVer}
+  </a>
+);
 
 export default () => (
   <Row type="flex" align="middle" style={styleRow}>
@@ -13,31 +54,11 @@ export default () => (
       <img src="assets/logo.svg" alt="MyQ Jobs Archive Parser" />
     </Col>
     <Col span={11} style={{ textAlign: 'right' }}>
-      Copyright &copy; 2018 Ivan Trofimov<br />
-      <a
-        href="#License"
-        onClick={e => {
-          e.preventDefault();
-          nw.Shell.openExternal(
-            'https://github.com/trofivan/myq-jobs-archive-parser/blob/master/LICENSE'
-          );
-        }}
-      >
-        MIT License
-      </a>
+      Copyright &copy; 2018 Ivan Trofimov
       <br />
-      <Icon type="github" />{' '}
-      <a
-        href="#GitHub"
-        onClick={e => {
-          e.preventDefault();
-          nw.Shell.openExternal(
-            'https://github.com/trofivan/myq-jobs-archive-parser'
-          );
-        }}
-      >
-        {appName}
-      </a>, v{appVer}
+      <UrlLicense />
+      <br />
+      <Icon type="github" /> <UrlProject />, <UrlLatestRelease />
     </Col>
   </Row>
 );
---
+++ src/constants/externalURLs.js
@@ -0,0 +1,6 @@
+export const URL_PROJECT =
+  'https://github.com/trofivan/myq-jobs-archive-parser';
+export const URL_LICENSE =
+  'https://github.com/trofivan/myq-jobs-archive-parser/blob/master/LICENSE';
+export const URL_LATEST_RELEASE =
+  'https://github.com/trofivan/myq-jobs-archive-parser/releases/latest';
